84065f96ef460c177e1e32ecfaab72e3d16e5fa3
Cadel Watson <cadel@cadelwatson.com>
2017-12-10 08:26:51 +1100

    Basic board representation

---
+++ constants.go
@@ -0,0 +1,17 @@
+package main
+
+// Piece codes
+const King = 0x03
+const Queen = 0x07
+const Rook = 0x05
+const Bishop = 0x04
+const Knight = 0x02
+const Pawn = 0x01
+const Empty = 0x00
+
+const White = 0x00
+const Black = 0x40
+
+const OffBoard = 0x88
+const Sliding = 0x04
+const Piece = 0x0F
--- main.go
+++ main.go
@@ -2,6 +2,79 @@ package main
 
 import "fmt"
 
+type position struct {
+	board [128]byte
+}
+
+var startBoard = [128]byte{
+	69, 66, 68, 71, 67, 68, 66, 69, 0, 0, 0, 0, 0, 0, 0, 0,
+	65, 65, 65, 65, 65, 65, 65, 65, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+	5, 2, 4, 7, 3, 4, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0,
+}
+
+func isSliding(piece byte) bool {
+	if piece&Sliding != 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func showPiece(piece byte) {
+	switch piece & Piece {
+	case King:
+		fmt.Print("K")
+	case Queen:
+		fmt.Print("Q")
+	case Rook:
+		fmt.Print("R")
+	case Bishop:
+		fmt.Print("B")
+	case Knight:
+		fmt.Print("H")
+	case Pawn:
+		fmt.Print("P")
+	default:
+		fmt.Print("_")
+	}
+}
+func showPosition(position position) {
+	for i := 0; i < 128; i++ {
+		if i&OffBoard == 0 {
+			showPiece(position.board[i])
+		}
+		if (i+1)%16 == 0 {
+			fmt.Print("\n")
+		}
+	}
+}
+
+func showSliding(position position) {
+	for i := 0; i < 128; i++ {
+		if i&OffBoard == 0 {
+			if isSliding(position.board[i]) {
+				fmt.Print("T")
+			} else {
+				fmt.Print("F")
+			}
+		}
+
+		if (i+1)%16 == 0 {
+			fmt.Print("\n")
+		}
+	}
+
+}
 func main() {
 	fmt.Printf("Hello, world\n")
+
+	startPosition := position{board: startBoard}
+
+	showPosition(startPosition)
+	showSliding(startPosition)
 }

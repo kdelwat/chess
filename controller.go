1aaa1d43ec1c7e0bc0d38d6e6892452a85834bf9
Cadel Watson <cadel@cadelwatson.com>
2017-12-11 11:59:45 +1100

    Handle commands in UCI

--- controller.go
+++ controller.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func startEngine() {
@@ -12,7 +13,30 @@ func startEngine() {
 	input, err := reader.ReadString('\n')
 
 	for err == nil {
-		fmt.Print(input)
+		handleCommand(input)
 		input, err = reader.ReadString('\n')
 	}
+}
+
+func handleCommand(command string) {
+	args := strings.Split(strings.TrimSpace(command), " ")
+
+	fmt.Printf("Args: %v\n", args)
+	switch args[0] {
+	case "uci":
+		handleUCI()
+	}
+}
+
+func handleUCI() {
+	sendCommand("id", "name", "Ultimate Engine")
+	sendCommand("id", "author", "Cadel Watson")
+}
+
+func sendCommand(command string, args ...string) {
+	tokens := append([]string{command}, args...)
+
+	outputCommand := strings.Join(tokens, " ")
+
+	fmt.Println(outputCommand)
 }

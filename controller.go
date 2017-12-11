abe9a4bd4a5ce4a2d9a49f76154b69a5daa9b9ff
Cadel Watson <cadel@cadelwatson.com>
2017-12-11 12:03:13 +1100

    Add debug command

--- controller.go
+++ controller.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+type globalOptions struct {
+	debug bool
+}
+
+var engineOptions = globalOptions{
+	debug: false,
+}
+
 func startEngine() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -25,9 +33,18 @@ func handleCommand(command string) {
 	switch args[0] {
 	case "uci":
 		handleUCI()
+	case "debug":
+		toggleDebug(args[1])
 	}
 }
 
+func toggleDebug(setting string) {
+	if setting == "on" {
+		engineOptions.debug = true
+	} else {
+		engineOptions.debug = false
+	}
+}
 func handleUCI() {
 	sendCommand("id", "name", "Ultimate Engine")
 	sendCommand("id", "author", "Cadel Watson")
@@ -39,4 +56,8 @@ func sendCommand(command string, args ...string) {
 	outputCommand := strings.Join(tokens, " ")
 
 	fmt.Println(outputCommand)
+}
+
+func sendDebug(message string) {
+	sendCommand("info", message)
 }

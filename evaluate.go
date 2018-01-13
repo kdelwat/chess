311939060f5d498b7d3e6c6466dfe578db594edf
Cadel Watson <cadel@cadelwatson.com>
2018-01-14 10:06:09 +1100

    Use more advanced evaluation method

--- evaluate.go
+++ evaluate.go
@@ -1,52 +1,85 @@
 package main
 
-const KingWeight = 100
-const QueenWeight = 9
-const RookWeight = 5
-const BishopWeight = 3
-const KnightWeight = 3
-const PawnWeight = 1
+// values and piece tables from https://chessprogramming.wikispaces.com/Simplified+evaluation+function
+const KingWeight = 10000
+const QueenWeight = 900
+const RookWeight = 500
+const BishopWeight = 300
+const KnightWeight = 300
+const PawnWeight = 100
+
+var pawnPositions = []int{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	50, 50, 50, 50, 50, 50, 50, 50,
+	10, 10, 20, 30, 30, 20, 10, 10,
+	5, 5, 10, 25, 25, 10, 5, 5,
+	0, 0, 0, 20, 20, 0, 0, 0,
+	5, -5, -10, 0, 0, -10, -5, 5,
+	5, 10, 10, -20, -20, 10, 10, 5,
+	0, 0, 0, 0, 0, 0, 0, 0,
+}
+
+var knightPositions = []int{
+	-50, -40, -30, -30, -30, -30, -40, -50,
+	-40, -20, 0, 0, 0, 0, -20, -40,
+	-30, 0, 10, 15, 15, 10, 0, -30,
+	-30, 5, 15, 20, 20, 15, 5, -30,
+	-30, 0, 15, 20, 20, 15, 0, -30,
+	-30, 5, 10, 15, 15, 10, 5, -30,
+	-40, -20, 0, 5, 5, 0, -20, -40,
+	-50, -40, -30, -30, -30, -30, -40, -50,
+}
+
+var bishopPositions = []int{
+	-20, -10, -10, -10, -10, -10, -10, -20,
+	-10, 0, 0, 0, 0, 0, 0, -10,
+	-10, 0, 5, 10, 10, 5, 0, -10,
+	-10, 5, 5, 10, 10, 5, 5, -10,
+	-10, 0, 10, 10, 10, 10, 0, -10,
+	-10, 10, 10, 10, 10, 10, 10, -10,
+	-10, 5, 0, 0, 0, 0, 5, -10,
+	-20, -10, -10, -10, -10, -10, -10, -20,
+}
+
+var rookPositions = []int{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	5, 10, 10, 10, 10, 10, 10, 5,
+	-5, 0, 0, 0, 0, 0, 0, -5,
+	-5, 0, 0, 0, 0, 0, 0, -5,
+	-5, 0, 0, 0, 0, 0, 0, -5,
+	-5, 0, 0, 0, 0, 0, 0, -5,
+	-5, 0, 0, 0, 0, 0, 0, -5,
+	0, 0, 0, 5, 5, 0, 0, 0,
+}
+
+var queenPositions = []int{
+	-20, -10, -10, -5, -5, -10, -10, -20,
+	-10, 0, 0, 0, 0, 0, 0, -10,
+	-10, 0, 5, 5, 5, 5, 0, -10,
+	-5, 0, 5, 5, 5, 5, 0, -5,
+	0, 0, 5, 5, 5, 5, 0, -5,
+	-10, 5, 5, 5, 5, 5, 0, -10,
+	-10, 0, 5, 0, 0, 0, 0, -10,
+	-20, -10, -10, -5, -5, -10, -10, -20,
+}
+
+var kingPositions = []int{
+	-30, -40, -40, -50, -50, -40, -40, -30,
+	-30, -40, -40, -50, -50, -40, -40, -30,
+	-30, -40, -40, -50, -50, -40, -40, -30,
+	-30, -40, -40, -50, -50, -40, -40, -30,
+	-20, -30, -30, -40, -40, -30, -30, -20,
+	-10, -20, -20, -20, -20, -20, -20, -10,
+	20, 20, 0, 0, 0, 0, 20, 20,
+	20, 30, 10, 0, 0, 10, 30, 20,
+}
 
 // return the negamax score for the given position. Eventually calculate this as
 // the same time as move making or generation
 // TODO: add mobility
 func evaluate(position position) int {
-	var kings int
-	var queens int
-	var rooks int
-	var bishops int
-	var knights int
-	var pawns int
 
-	for i := 0; i < BoardSize; i++ {
-
-		piece := position.board[i]
-
-		if isOnBoard(i) && position.board[i].exists() {
-			var increment int
-			if piece.color() == White {
-				increment = 1
-			} else {
-				increment = -1
-			}
-
-			switch piece.identity() {
-			case King:
-				kings += increment
-			case Queen:
-				queens += increment
-			case Bishop:
-				bishops += increment
-			case Rook:
-				rooks += increment
-			case Knight:
-				knights += increment
-			case Pawn:
-				pawns += increment
-
-			}
-		}
-	}
+	var score int
 
 	var direction int
 	if position.toMove == White {
@@ -55,7 +88,47 @@ func evaluate(position position) int {
 		direction = -1
 	}
 
-	// formula from https://chessprogramming.wikispaces.com/Evaluation
-	return (KingWeight*kings + QueenWeight*queens + RookWeight*rooks + BishopWeight*bishops + KnightWeight*knights + PawnWeight*pawns) * direction
+	for i := 0; i < BoardSize; i++ {
 
+		piece := position.board[i]
+		if isOnBoard(i) && position.board[i].exists() {
+			var increment int
+			if piece.color() == White {
+				increment = 1
+			} else {
+				increment = -1
+			}
+
+			piecemapIndex := map0x88ToPiecemap(i, increment)
+
+			switch piece.identity() {
+			case King:
+				score += (KingWeight + kingPositions[piecemapIndex]) * increment
+			case Queen:
+				score += (QueenWeight + queenPositions[piecemapIndex]) * increment
+			case Bishop:
+				score += (BishopWeight + bishopPositions[piecemapIndex]) * increment
+			case Rook:
+				score += (RookWeight + rookPositions[piecemapIndex]) * increment
+			case Knight:
+				score += (KnightWeight + knightPositions[piecemapIndex]) * increment
+			case Pawn:
+				score += (PawnWeight + pawnPositions[piecemapIndex]) * increment
+			}
+		}
+	}
+
+	return score * direction
+
+}
+
+func map0x88ToPiecemap(index int, direction int) uint {
+	rank := index / 16
+	file := index % 16
+
+	if direction == 1 {
+		rank = 7 - rank
+	}
+
+	return uint(rank*8 + file)
 }

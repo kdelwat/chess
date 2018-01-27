708996a2c545101942975921da4cb02d647cfd03
Cadel Watson <cadel@cadelwatson.com>
2018-01-28 09:45:22 +1100

    Comment make move

--- make.go
+++ make.go
@@ -1,5 +1,9 @@
 package main
 
+/*
+When making a move, some information about the previous state cannot be
+recovered from the next state. The moveArtifacts type contains this information.
+*/
 type moveArtifacts struct {
 	halfmove          byte
 	castling          byte
@@ -7,6 +11,8 @@ type moveArtifacts struct {
 	captured          piece
 }
 
+// Makes a quiet move (a regular move with no captures) given the position,
+// origin, and destination.
 func makeQuietMove(position *position, from byte, to byte) {
 	pieceMoved := position.board[from]
 
@@ -14,8 +20,17 @@ func makeQuietMove(position *position, from byte, to byte) {
 	position.board[to] = pieceMoved
 }
 
+/*
+Performs a move on the given position. This function takes a pointer to the
+current position, so it modifies it in-place. While this isn't ideal from a
+debugging point of view, it would be impossible to copy the position each
+time due to memory constraints.
+
+makeMove returns the artifacts required to reverse the move later.
+*/
 func makeMove(position *position, move move) moveArtifacts {
 
+	// Record the current state in artifacts.
 	var artifacts = moveArtifacts{
 		halfmove:          position.halfmove,
 		castling:          position.castling,
@@ -23,15 +38,17 @@ func makeMove(position *position, move move) moveArtifacts {
 		captured:          0,
 	}
 
+	// The new position by default has no en passant target.
 	position.enPassantTarget = NoEnPassant
 
+	// The halfmove counter is rest on a capture, and incremented otherwise.
 	if move&Capture != 0 {
 		position.halfmove = 0
 	} else {
 		position.halfmove++
 	}
 
-	// If the king or rook are moving, remove castle rights
+	// If the king or rook are moving, remove castling rights
 	if position.board[move.From()].is(King) {
 		position.castling = setCastle(position.castling, KingCastle, position.toMove, false)
 		position.castling = setCastle(position.castling, QueenCastle, position.toMove, false)
@@ -49,17 +66,21 @@ func makeMove(position *position, move move) moveArtifacts {
 		}
 	}
 
+	// Determine which type of move to make.
 	if move.isQuiet() {
 		makeQuietMove(position, move.From(), move.To())
 
+		// A quiet move resets the halfmove counter if it is made by a pawn.
 		if position.board[move.From()].is(Pawn) {
 			position.halfmove = 0
 		}
 
 	} else if move.isCastle() {
+		// If the player is castling, remove all castle rights in the future.
 		position.castling = setCastle(position.castling, KingCastle, position.toMove, false)
 		position.castling = setCastle(position.castling, QueenCastle, position.toMove, false)
 
+		// Determine the starting and ending location of the pieces involved.
 		var kingOrigin int
 		var rookOrigin int
 		var kingFinal int
@@ -81,6 +102,7 @@ func makeMove(position *position, move move) moveArtifacts {
 			rookFinal = kingOrigin + 1
 		}
 
+		// Swap the pieces in the board.
 		king := position.board[kingOrigin]
 		rook := position.board[rookOrigin]
 		position.board[kingOrigin] = 0
@@ -93,6 +115,8 @@ func makeMove(position *position, move move) moveArtifacts {
 
 		if move.isPromotionCapture() {
 			promotionPiece := move.getPromotedPiece(pieceMoved)
+
+			// Save the captured piece in the move artifacts.
 			artifacts.captured = position.board[move.To()]
 
 			position.board[move.From()] = 0
@@ -103,11 +127,14 @@ func makeMove(position *position, move move) moveArtifacts {
 			position.board[move.From()] = 0
 			position.board[move.To()] = promotionPiece
 
+			// The halfmove counter is reset on a promotion.
 			position.halfmove = 0
 		} else if move.isEnPassantCapture() {
 			position.board[move.From()] = 0
 			position.board[move.To()] = pieceMoved
 
+			// Determine the en passant target, depending on the direction of
+			// movement.
 			var captureIndex int
 			if pieceMoved.color() == White {
 				captureIndex = int(move.To()) - 16
@@ -121,6 +148,8 @@ func makeMove(position *position, move move) moveArtifacts {
 			position.board[move.From()] = 0
 			position.board[move.To()] = pieceMoved
 
+			// A double pawn push creates an en passant target, which must be
+			// saved in the new position.
 			position.enPassantTarget = byte(move.From()+move.To()) / 2
 			position.halfmove = 0
 		} else if move.isCapture() {
@@ -132,8 +161,7 @@ func makeMove(position *position, move move) moveArtifacts {
 
 	}
 
-	// Check if the rook was captured for castling purposes
-	// OPT
+	// If the rook was captured, remove castling rights for that side.
 	if artifacts.captured != 0 && artifacts.captured.is(Rook) {
 		color := artifacts.captured.color()
 
@@ -154,6 +182,7 @@ func makeMove(position *position, move move) moveArtifacts {
 		}
 	}
 
+	// Increment the fullmove counter when black finishes their turn.
 	if position.toMove == White {
 		position.toMove = Black
 	} else {
@@ -164,11 +193,18 @@ func makeMove(position *position, move move) moveArtifacts {
 	return artifacts
 }
 
+/*
+Reverses a move on the given position. This function takes a position, the move
+which was applied, and the artifacts generated by makeMove, and restores the
+position in-place to the state before the move was applied.
+*/
 func unmakeMove(position *position, move move, artifacts moveArtifacts) {
+	// Restore state information from artifacts.
 	position.halfmove = artifacts.halfmove
 	position.castling = artifacts.castling
 	position.enPassantTarget = artifacts.enPassantPosition
 
+	// Decrement the fullmove counter if black made the last move.
 	if position.toMove == White {
 		position.fullmove--
 		position.toMove = Black
@@ -182,6 +218,7 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 		position.board[move.To()] = 0
 		position.board[move.From()] = pieceMoved
 	} else if move.isCastle() {
+		// Determine the starting and ending location of the pieces involved.
 		var kingOrigin int
 		var rookOrigin int
 		var kingFinal int
@@ -203,6 +240,7 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 			rookFinal = kingOrigin + 1
 		}
 
+		// Swap the pieces in the board.
 		king := position.board[kingFinal]
 		rook := position.board[rookFinal]
 		position.board[kingFinal] = 0
@@ -214,7 +252,8 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 		pieceMoved := position.board[move.To()]
 
 		if move.isPromotionCapture() {
-			// recreate pawn
+			// If the move was a promotion capture, recreate the pawn piece that
+			// was replaced by the promoted piece.
 			var pawn piece
 			pawn |= piece(pieceMoved.color())
 			pawn |= Pawn
@@ -222,7 +261,8 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 			position.board[move.From()] = pawn
 			position.board[move.To()] = artifacts.captured
 		} else if move.isPromotion() {
-			// recreate pawn
+			// If the move was a promotion , recreate the pawn piece that was
+			// replaced by the promoted piece.
 			var pawn piece
 			pawn |= piece(pieceMoved.color())
 			pawn |= Pawn
@@ -233,6 +273,8 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 			position.board[move.To()] = 0
 			position.board[move.From()] = pieceMoved
 
+			// Determine the index captured by the en passant, depending on the
+			// direction of movement.
 			var captureIndex int
 			if pieceMoved.color() == White {
 				captureIndex = int(move.To()) - 16
@@ -240,6 +282,7 @@ func unmakeMove(position *position, move move, artifacts moveArtifacts) {
 				captureIndex = int(move.To()) + 16
 			}
 
+			// Restore the captured piece.
 			position.board[captureIndex] = artifacts.captured
 		} else if move.isDoublePawnPush() {
 			position.board[move.To()] = 0

a0775e86818061e02285cd798522335b126e819d
Cadel Watson <cadel@cadelwatson.com>
2018-01-28 15:33:55 +1100

    Comment pieces

--- piece.go
+++ piece.go
@@ -1,23 +1,55 @@
 package main
 
+/*
+A piece is represented by a single byte.
+
+          off the board
+          +       +
+          |       | +----+sliding
+          v       v v
+          _ _ _ _ _ _ _ _
+            ^ ^     ^ ^ ^
+  colour+---+ |     + + +
+              |     identity
+double pushed++
+
+If the piece is white, the colour bit is 0. Otherwise, it is 1.
+
++--------+----------+
+| Piece  | Identity |
++--------+----------+
+| empty  |      000 |
+| pawn   |      001 |
+| knight |      010 |
+| bishop |      100 |
+| rook   |      101 |
+| king   |      011 |
+| queen  |      111 |
++--------+----------+
+*/
 type piece byte
 
+// Is the piece a sliding piece (bishop, rook, and queen)?
 func (p piece) isSliding() bool {
 	return (p&Sliding != 0)
 }
 
+// Is the piece the same as the target type?
 func (p piece) is(target piece) bool {
 	return (p&Piece == target)
 }
 
+// Is there a piece on the square?
 func (p piece) exists() bool {
 	return (p&Piece != 0)
 }
 
+// Find the colour of the piece.
 func (p piece) color() byte {
 	return byte(p & Color)
 }
 
+// Find the type of the piece.
 func (p piece) identity() piece {
 	return p & Piece
 }
